4a141a5bd710bf744398b489ab4b0675dcc34254
kokoyo1997 <15071367672@163.com>
2021-08-28 19:07:17 +0800

    图片拉伸

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -9,14 +9,15 @@ function Photos(){
                 {photoLists.map((zone)=>(
                     <li key={zone.id}>
                         <h3 className="pl-2 border-l-4 border-gray-700 text-gray-700 text-lg font-bold mb-4">{zone.name}</h3>
-                        <ul className="flex flex-wrap">
+                        <ul className="flex flex-wrap items-stretch">
                             {zone.lists.map(ele=>(
                                 <li className="mr-4 mb-4" key={ele.id}>
                                     <Link to={ele.url} className="space-y-2">
-                                        <img src={REPO+ele.path} className="w-44 border-dotted border-2 border-gray-400" alt={ele.title}></img>
+                                        <img src={REPO+ele.path} className="w-44 border-dotted border-2 border-gray-400 h-full" alt={ele.title}></img>
                                         <p className="text-center text-gray-500 text-sm">「{ele.title}」</p>
                                     </Link>
                                 </li>
+
                             ))}
                             
                             
