d17826bb56f18a8798af71c24d89733d401a3091
xly761 <15071367672@163.com>
2021-09-12 16:56:59 +0800

    照片更新

--- src/assets/photos.js
+++ src/assets/photos.js
@@ -31,7 +31,7 @@ export const imageLists = [{
     }
 ];
 
-export const photoLists = ["yys","onepiece"];// 照片显示页面分区列表
+export const photoLists = ["yys","onepiece","life"];// 照片显示页面分区列表
 
 export const albums={
     "yys":{
@@ -157,6 +157,91 @@ export const albums={
                 },
             ]
         }
+    },
+    "life":{
+        id:3,
+        title:"生活日常Life",
+        lists:["1"],
+        1:{
+            id:300,
+            title:"拼拼豆豆",
+            cover:"/uploads/albums/3/1/1631436226143.jpeg",
+            url: "/albums/life/1",
+            list:[
+                {
+                    id: 1,
+                    name: "茨球",
+                    path: "/uploads/albums/3/1/1631436226143.jpeg"
+                },
+                {
+                    id: 2,
+                    name: "青行灯",
+                    path: "/uploads/albums/3/1/1631436228394.jpeg"
+                },
+                {
+                    id: 3,
+                    name: "达摩",
+                    path: "/uploads/albums/3/1/1631436198907.jpeg"
+                },
+                {
+                    id: 4,
+                    name: "祢豆子-站",
+                    path: "/uploads/albums/3/1/1631436204111.jpeg"
+                },
+                {
+                    id: 5,
+                    name: "祢豆子-坐",
+                    path: "/uploads/albums/3/1/1631436209080.jpeg"
+                },
+                {
+                    id: 6,
+                    name: "炭治郎",
+                    path: "/uploads/albums/3/1/1631436233110.jpeg"
+                },
+                {
+                    id: 7,
+                    name: "祢豆子",
+                    path: "/uploads/albums/3/1/1631436235392.jpeg"
+                },
+                
+                {
+                    id: 8,
+                    name: "善逸",
+                    path: "/uploads/albums/3/1/1631436245278.jpeg"
+                },
+                {
+                    id: 9,
+                    name: "伊之助",
+                    path: "/uploads/albums/3/1/1631436251709.jpeg"
+                },
+                {
+                    id: 10,
+                    name: "香奈乎",
+                    path: "/uploads/albums/3/1/1631436248241.jpeg"
+                },
+                {
+                    id: 10,
+                    name: "香奈乎",
+                    path: "/uploads/albums/3/1/1631436248241.jpeg"
+                },
+                {
+                    id: 11,
+                    name: "我的世界",
+                    path: "/uploads/albums/3/1/IMG_20200214_154045R.jpg"
+                },
+                {
+                    id: 12,
+                    name: "阿米娅",
+                    path: "/uploads/albums/3/1/1631436194642.jpeg"
+                },
+                {
+                    id: 13,
+                    name: "小电视",
+                    path: "/uploads/albums/3/1/IMG_20200211_215435R.jpg"
+                },
+            ]
+        }
     }
+
 }
 
